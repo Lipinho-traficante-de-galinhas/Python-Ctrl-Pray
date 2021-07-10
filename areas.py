3fa6e2b674bd74ac7dfc0460677c354b6805a25a
lipinho-traficante-de-galinhas <felipecesarestudos@gmail.com>
2021-07-10 10:56:57 -0300

    Young 1 = Aula 9,

---
+++ areas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def areaQuadrado(lado):
+    area = lado * lado
+    return  area
+
+def areaTriangulo(base, altura):
+    area = base * altura / 2
+    return area
+
+def areaTrapezio(b,B,h):
+    area = ((b+b)*h)/2
+    return area
+
